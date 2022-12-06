99c43ca77757b73b8bef2ba389213136f195cfbe
Alamin <alaminshaheen@iut-dhaka.edu>
2022-12-07 01:28:01 +0600

    day_6

---
+++ Day_6/task_1.py
@@ -0,0 +1,14 @@
+for line in open("input.txt", "r"):
+    line = line.strip()
+    left = 0
+    window = set()
+    for right in range(len(line)):
+        while line[right] in window:
+            window.remove(line[left])
+            left += 1
+
+        window.add(line[right])
+
+        if len(window) == 4:
+            print(right + 1)
+            break
