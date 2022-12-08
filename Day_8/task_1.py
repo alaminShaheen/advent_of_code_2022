6d84fe05a66a64beb965d0238dfe40ee54c6d42f
Alamin <alaminshaheen@iut-dhaka.edu>
2022-12-08 22:46:39 +0600

    day_8

--- Day_8/task_1.py
+++ Day_8/task_1.py
@@ -1,27 +1,24 @@
-row = 0
+grid = []
+
+
+def find_visibility(row: int, col: int) -> int:
+    tree = grid[row][col]
+
+    top = left = right = bottom = 0
+    for c in range(col + 1, len(grid[0])):
+        if grid[row][c] < tree:
+            right += 1
+        elif grid[row][c] >= tree:
+            right += 1
+            break
+
+
+
+
 for line in open("input.txt", "r"):
-    line = list(map(int, line.strip().split()))
-    left, right = 0, len(line) - 1
-    visible_trees = 0
-    visible_trees_coord = set()
-    left_big, right_big = line[left], line[right]
+    line = list(map(int, list(line.strip())))
+    grid.append(line)
 
-    while left < len(line) or right >= 0:
-        if (row, left) not in visible_trees_coord or left == 0 or line[left] > left_big:
-            visible_trees += 1
-            left_big = max(left_big, line[left])
-            visible_trees_coord.add((row, left))
-            continue
-        else:
-            left_big = math.inf
-
-        if (row, right) not in visible_trees_coord or right == len(line) - 1 or line[right] > right_big:
-            visible_trees += 1
-            right_big = max(right_big, line[right])
-            visible_trees_coord.add((row, right))
-            continue
-        else:
-            right_big = math.inf
-        left += 1
-        right -= 1
-    row += 1
+for row in range(len(grid)):
+    for col in range(len(grid[0])):
+        find_visibility(row, col)
--- Day_8/task_2.py
+++ Day_8/task_2.py
@@ -1,71 +1,41 @@
 import math
 
-
-class File:
-    def __init__(self, name, size, parent_directory):
-        self.name = name
-        self.size = size
-        self.parent = parent_directory
-        self.type = "file"
+grid = []
 
 
-class Directory(File):
-    def __init__(self, name, size, parent_directory, children=None):
-        super().__init__(name, size, parent_directory)
-        self.children = children if children is not None else {}
-        self.type = "directory"
+def find_visibility(row: int, col: int) -> int:
+    tree = grid[row][col]
 
+    top = left = right = bottom = 0
+    for c in range(col + 1, len(grid[0])):
+        right += 1
+        if grid[row][c] >= tree:
+            break
 
-limit, disk_space = 100000, 70000000
-root_dir = Directory("/", 0, None, {})
-parent_dir: Directory = None
-current_dir: Directory = root_dir
+    for c in range(col - 1, -1, -1):
+        left += 1
+        if grid[row][c] >= tree:
+            break
+
+    for r in range(row + 1, len(grid)):
+        bottom += 1
+        if grid[r][col] >= tree:
+            break
+
+    for r in range(row - 1, -1, -1):
+        top += 1
+        if grid[r][col] >= tree:
+            break
+    return top * right * bottom * left
+
 
 for line in open("input.txt", "r"):
-    instruction = line.strip().split(" ")
+    line = list(map(int, list(line.strip())))
+    grid.append(line)
 
-    if instruction[0] == "$":
-        if instruction[1] == "cd":
-            _, command, destination = instruction
+max_visibility = -math.inf
+for row in range(len(grid)):
+    for col in range(len(grid[0])):
+        max_visibility = max(max_visibility, find_visibility(row, col))
 
-            if destination == "..":
-                current_dir = current_dir.parent
-            else:
-                current_dir = root_dir if root_dir.name == destination else current_dir.children[destination]
-            parent_dir = current_dir.parent
-    else:
-        if instruction[0] == "dir":
-            _, directory_name = instruction
-            directory = Directory(directory_name, 0, current_dir, {})
-            current_dir.children[directory.name] = directory
-        else:
-            # print(instruction, current_dir.name, parent_dir.name if parent_dir is not None else None)
-            file_size, file_name = instruction
-            file = File(file_name, int(file_size), current_dir)
-            current_dir.children[file.name] = file
-
-directory_sizes = []
-
-
-def get_dir_sizes(directory: Directory):
-    dir_size = 0
-    for file in directory.children.values():
-        if file.type == "file":
-            dir_size += file.size
-        else:
-            dir_size += get_dir_sizes(file)
-    directory.size = dir_size
-
-    directory_sizes.append(directory.size)
-
-    return directory.size
-
-
-get_dir_sizes(root_dir)
-
-meen = math.inf
-for size in directory_sizes:
-    if disk_space - root_dir.size + size >= 30000000:
-        meen = min(meen, size)
-
-print(meen)
+print(max_visibility)
